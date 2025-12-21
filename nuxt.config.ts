23da0df6ef1d780e1f5b6b7a47120d79e1c07420
Kenyi <kenyiteran5@gmail.com>
2025-12-21 17:41:41 -0500

    update

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'es'
+      }
+    }
+  }
+})
