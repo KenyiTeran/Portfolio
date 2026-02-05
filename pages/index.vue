5305aaec82ff92ad9d09b4593d30d1f1f19853a0
Kenyi <kenyiteran5@gmail.com>
2026-02-04 23:33:35 -0500

    Prettier configuration

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="pt-40"> 
-        <h1 class="bg-red-200">Portafolio</h1>
-        <KenyoAvatar />
-    </div>
+  <div class="">
+    <h1 class="bg-red-200">Portafolio</h1>
+    <!-- <KenyoAvatar /> -->
+  </div>
 </template>
