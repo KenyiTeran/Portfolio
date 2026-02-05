87aec662d71197cb8af1f4dbe1f5e1bdd2b1df96
Kenyi <kenyiteran5@gmail.com>
2026-02-04 23:11:33 -0500

    Install Pruvious

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,17 +4,25 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', "pruvious"],
+
   vite: {
     plugins: [
       tailwindcss(),
     ]
   },
+
   app: {
     head: {
       htmlAttrs: {
         lang: 'es'
       }
+    }
+  },
+
+  pruvious: {
+    jwt: {
+      secretKey: "J69u5jpcyYF35Hc_Nbkv-FneCGvut1uRh77hbAtC8kr5_j3a8wsQ-ucV_d7Jv3lD"
     }
   }
 })
